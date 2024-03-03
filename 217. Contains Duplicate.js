9828b8b1ec0ce0b5f546c21609699b487c705170
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2024-03-03 15:45:42 +0600

    217- Hash Set

--- 217. Contains Duplicate.js
+++ 217. Contains Duplicate.js
@@ -42,3 +42,43 @@ var containsDuplicate = (nums) => {
 
   return false;
 };
+
+
+/**
+ * Sort - HeapSort Space O(1) | QuickSort Space O(log(N))
+ * Time O(N * log(N)) | Space O(1)
+ * https://leetcode.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+    nums.sort((a, b) => a - b);/* Time O(N * log(N)) | Space O(1 || log(N)) */
+
+    return hasDuplicate(nums);
+}
+
+const hasDuplicate = (nums) => {
+    for (let curr = 0; curr < (nums.length - 1); curr++) {/* Time O(N) */
+        const next = (curr + 1);
+
+        const isNextDuplicate = nums[curr] === nums[next];
+        if (isNextDuplicate) return true;
+    }
+
+    return false;
+}
+
+
+/**
+ * Hash Set
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+    const numsSet = new Set(nums);/* Time O(N) | Space O(N) */
+    const isEqual = numsSet.size === nums.length;
+
+    return !isEqual;
+};
