44b4fdc623716d0ea0d8b71634cbadc2e5886a59
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2024-03-03 16:06:09 +0600

    Hash Set 217

--- 217. Contains Duplicate.js
+++ 217. Contains Duplicate.js
@@ -92,3 +92,19 @@ var containsDuplicate = function(nums) {
     } else return true
 };
 */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    let numsSet = new Set()
+    for (const num of nums) {
+        if (numsSet.has(num)) return true
+
+        numsSet.add(num)
+    }
+
+    return false
+};
