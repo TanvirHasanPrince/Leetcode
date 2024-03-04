e9ff5d7d6c028be58ae44abf7e8e674ad5ecf736
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-04 23:12:34 +0600

    two sum

---
+++ 1. Two sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let curr = 0; curr < nums.length; curr++) {
+    /* Time O(N) */
+    const complement = target - nums[curr];
+
+    for (let next = curr + 1; next < nums.length; next++) {
+      /* Time O(N) */
+      const num = nums[next];
+
+      const isTarget = num === complement;
+      if (isTarget) return [curr, next];
+    }
+  }
+
+  return [-1, -1];
+};
